bf2969fa36a3493c2057027a5015799e2446fdcd
Ian <ian.tehwj@gmail.com>
2020-09-03 12:25:37 +0800

    Added NavigationBar to components

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Landing from './components/landing.js'
 
-import Register from './components/register'
-import Login from './components/login.js'
+
+import { NavigationBar } from './components/NavigationBar';
+
+import Register from './components/register';
+import Login from './components/login.js';
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
           
-          <Route exact path="/" component={Landing} />
+          <Route exact path="/" component={NavigationBar} />
           <div className="container">
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
---
+++ client/src/components/NavigationBar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Nav, Navbar, Dropdown} from 'react-bootstrap';
+import styled from 'styled-components';
+const Styles = styled.div`
+  .navbar { background-color: #365; }
+  a, .navbar-nav, .navbar-light .nav-link {
+    color: #000000;
+    &:hover { color: white; }
+  }
+  .navbar-brand {
+    font-size: 1.4em;
+    color: #000000;
+    &:hover { color: white; }
+  }
+  .dropdown-center {
+    position: absolute !important;
+    left: 50%;
+    right: 50%;
+  }
+  .color-nav {
+      background-color : rgb(255,255,255);
+  }
+`;export const NavigationBar = () => (
+  <Styles>  
+    <Navbar className = "color-nav" expand="lg">
+      <Navbar.Brand href="/">SwatKats</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Item><Nav.Link href="/register">Register</Nav.Link></Nav.Item> 
+          <Nav.Item><Nav.Link href="/login">Login</Nav.Link></Nav.Item>
+          <Dropdown class = "dropdown-center">
+                <Dropdown.Toggle variant = "info" id = "dropdown-basic">
+                    Language Options
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                </Dropdown.Menu>
+          </Dropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </Styles>
+)
