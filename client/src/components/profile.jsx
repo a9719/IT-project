6aa7aa7f5c164a6c91ff9104079181015969b3d1
Dylanste <62159446+Dylanste@users.noreply.github.com>
2020-09-26 17:05:22 +1000

    Profile page with structured cards and Accordian drop downs. no visual style

--- client/src/components/profile.jsx
+++ client/src/components/profile.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import {Helmet} from "react-helmet";
 import axios from 'axios';
 import {connect} from 'react-redux';
-import { Nav, Navbar, Dropdown} from 'react-bootstrap';
+import { Nav, Navbar, Dropdown, Card, CardGroup, Accordion, AccordionToggle} from 'react-bootstrap';
 import styled from 'styled-components';
 import logo from './logo.svg';
 import PropTypes from 'prop-types';
@@ -11,8 +11,11 @@ import { setCurrentUser, logoutUser } from "./../actions/authActions";
 import JwtDecode from 'jwt-decode';
 import setAuthToken from "../utils/setAuthToken"
 import { GET_PROFILE } from "../actions/profileActions";
-import NavigationBar from "./components/NavigationBar"
 import Footer from "./Footer";
+import NavigationBar from "./NavigationBar"
+
+
+import image from './blank-profile.png';
 
 class Profile extends Component {
   constructor(props) {
@@ -57,8 +60,76 @@ class Profile extends Component {
             <h1 className="text-center">WELCOME {this.state.email} </h1>
             <h2 className = "text-center">Welcome {this.state.bio} </h2>
           </div>
-          
         </div>
+        <div class = "row">
+            <div class="col-md-3"></div>
+                <CardGroup>
+                    <Card style={{ width: '30rem' }}>
+                        <Card.Header> Jonh Doe </Card.Header>
+                        <Card.Body>
+                            <Card.Text> Bio goes here</Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card style={{ width: '30rem' }}>
+                        <Card.Img src={image} alt= "Card image"/>
+                    </Card>
+                </CardGroup>
+          </div>
+          <div class = "row">
+            <div class="col-md-3"></div>
+            <CardGroup>
+                    <Card style={{ width: '20rem' }}>
+                      <Card.Header> Accademics </Card.Header>
+                    </Card>
+                    <Card style={{ width: '20rem' }}>
+                      <Card.Header> Skills </Card.Header>
+                    </Card>
+                    <Card style={{ width: '20rem' }}>
+                      <Card.Header> Projects </Card.Header>
+                    </Card>
+                </CardGroup>
+          </div>
+          {/*1: this is apparently how you comment seems overly complicated
+          2: i am unsure as to how to properly format the cards and what not to look good. Dylan*/}
+          <div class = "row">
+            <div class="col-md-3"></div>
+              <Accordion>
+                <Card style={{ width: '60rem' }}>
+                  <Accordion.Toggle as = {Card.Header} eventKey = '0'>
+                    Accademics
+                  </Accordion.Toggle>
+                  <Accordion.Collapse eventKey="0">
+                    <Card.Body>this should be a list of Accademic pages</Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+              </Accordion>
+          </div>
+          <div class = "row">
+            <div class="col-md-3"></div>
+              <Accordion>
+                <Card style={{ width: '60rem' }}>
+                  <Accordion.Toggle as = {Card.Header} eventKey = '0'>
+                    Skills
+                  </Accordion.Toggle>
+                  <Accordion.Collapse eventKey="0">
+                    <Card.Body>this should be a list of Skill pages</Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+              </Accordion>
+          </div>
+          <div class = "row">
+            <div class="col-md-3"></div>
+              <Accordion>
+                <Card style={{ width: '60rem' }}>
+                  <Accordion.Toggle as = {Card.Header} eventKey = '0'>
+                    Projects
+                  </Accordion.Toggle>
+                  <Accordion.Collapse eventKey="0">
+                    <Card.Body>this should be a list of Project pages</Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+              </Accordion>
+          </div>
         <Footer/>
       </div>
     )
