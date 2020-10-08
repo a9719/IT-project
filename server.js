cda74c86a2068dbbedca580fc96dd0ee58e39d66
aneesh <achattaraj@student.unimelb.edu.au>
2020-10-09 02:35:17 +1100

    changes to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var cors = require('cors')
 var bodyParser = require('body-parser')
+const path = require('path')
 var app = express()
 const mongoose = require('mongoose')
 var port = process.env.PORT || 5000
@@ -27,7 +28,10 @@ require('./models/profile.js');
 
 const routes = require('./routes/router.js');
 const fileRoutes = require('./routes/file-upload.js');
-
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
+})
 
 app.use('/', routes);
 app.use('/', fileRoutes);
