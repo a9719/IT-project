db70d979a5779017a14e3e15d01cac3f7ce5e18b
tmofveins <nicksiow.zj@gmail.com>
2020-10-09 18:53:20 +0800

    attempt to fix heroku deployment issues

--- server.js
+++ server.js
@@ -38,3 +38,13 @@ app.use('/static', express.static(path.join(__dirname, 'client/build')));
 app.listen(port, function() {
   console.log('Server is running on port: ' + port)
 })
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'frontend/build')));
+    
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+  });
+}
