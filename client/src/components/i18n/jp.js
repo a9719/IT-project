29f78f8c98ab2430c9e08d29c9f3bc4df159bfe1
aneesh <achattaraj@student.unimelb.edu.au>
2020-10-25 13:12:35 +1100

    jap translations

--- client/src/components/i18n/jp.js
+++ client/src/components/i18n/jp.js
@@ -1,17 +1,37 @@
 export default{
-    Im:"わたし",
-    home:"家",
-    education:"教育",
-    skills:"腕前",
-    projects:"プロジェクト",
-    subjects:"科目",
-    work:"職歷",
-    contact:"連絡",
-    language:"言語",
-    logout:"ログアウト",
-    upload:"アップロード",
-    about_me:"私について",
-    contact_details:"連絡の詳細",
-    add_subjects:"件名の追加",
-    delete:"削る"
-  }
+  Im:"私は",
+  home:"ホームページ",
+  education:"教育",
+  skills:"腕前",
+  projects:"プロジェクト",
+  subjects:"科目",
+  work:"職歷",
+  contact:"連絡",//contact username more natural than contact me. can use "連絡をお願いします" (please contact me)
+  language:"言語",
+  logout:"ログアウト",
+  upload:"アップロード",
+  about_me:"私について",
+  contact_details:"連絡先",　// 連絡先 is a common phrase for contact infomation vs 連絡の詳細 which directly translates to contact's details.
+  add_subjects:"科目に追加",　// directly translates to "add to subjects" rather than "add subject" which makes a little more sense in japanesee now
+  delete:"削除",
+  edit_Intro:"概論を編集", //translates roughly to edit general remarks. I can change the work to edit self introduction if that is more fitting. 
+  work1: "職歴", 
+  add_work:"職歴に追加",
+  welcome:"ウエルカム",
+  login:"ログイン",
+  register:"サインアップ", // more common to you "sign up" in japanese
+  createdby:"作成者", //translates roughly to "creaters" rather than "created by"
+  Createacc:"アカウントを作成する",
+  registerfa:"もうアカウントをもっていますか？",
+  submit:"送信",
+  edit_Bio:"伝記を編集",
+  edit_phone:"電話番号を編集",
+  upload_transcript:"トランスクリプトをアップロード",
+  download:"成績書をダウンロードするにはクリックしてください",
+  add_edu:"教育を追加",
+  add_projects:"プロジェクトを追加",
+  add_skills:"腕前を追加",
+  gallery:"ギャラリー",
+  add_photodesc:"写真の説明を追加",
+  add_photo:"写真をアップロード"
+}
